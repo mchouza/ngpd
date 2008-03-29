165e5ec67a24fde2788bc32d29e4936e85e26bed
mchouza <mchouza@b858013c-4649-0410-a850-dde43e08a396>
2008-03-29 17:17:58 +0000

    - Carga de módulos completa, aunque todavía no probada (ngpd_modules.cpp). - Módulos adaptados para funcionar con shared_ptr en lugar de Poco::SharedPtr.

--- trunk/new_gp_daemon/ngpd_modules.cpp
+++ trunk/new_gp_daemon/ngpd_modules.cpp
@@ -35,10 +35,13 @@
 // Creado por Mariano M. Chouza | Empezado el 28 de marzo de 2008
 //=============================================================================
 
+#include "exceptions.h"
 #include "ngpd_modules.h"
 #include "os_dep.h"
 #include <module.h>
+#include <Poco/ClassLoader.h>
 #include <Poco/DirectoryIterator.h>
+#include <Poco/SharedLibrary.h>
 #include <Poco/Util/Application.h>
 
 using namespace Core;
@@ -65,9 +68,33 @@ namespace
 	}
 
 	/// Carga un módulo especificado por su path
-	void loadModule(const std::string& modulePath)
+	void loadModule(std::vector<boost::shared_ptr<Module> >& modules,
+		const std::string& modulePath)
 	{
-		// FIXME: Hacer que funcione
+		using boost::shared_ptr;
+		using Poco::AbstractMetaObject;
+		using Poco::ClassLoader;
+		typedef Poco::ClassLoader<Module>::Manif TManif;
+		typedef TManif::Iterator TManifIt;
+		
+		// Creo el ClassLoader
+		ClassLoader<Module> loader;
+
+		// Cargo el módulo y obtengo el "manifest"
+		loader.loadLibrary(modulePath);
+		const TManif& manif = *loader.findManifest(modulePath);
+
+		// Recorro el manifest
+		TManifIt it = manif.begin();
+		TManifIt itEnd = manif.end();
+		for (; it != itEnd; ++it)
+		{
+			// Obtengo el MetaObject
+			const AbstractMetaObject<Module>& meta = **it;
+
+			// Guardo el puntero correspondiente
+			modules.push_back(shared_ptr<Module>(meta.create()));
+		}
 	}
 
 	/// Carga los módulos en un directorio dado
@@ -82,9 +109,8 @@ namespace
 		vector<string> filesInDir = getAllFilesInDir(dirPath);
 
 		// Cargo todos los módulos
-		Application::instance().logger();
 		for (size_t i = 0; i < filesInDir.size(); i++)
-			loadModule(filesInDir[i]);
+			loadModule(modules, filesInDir[i]);
 	}
 
 	/// Trata de cumplir con las dependencias de todos los módulos. En caso de
@@ -92,7 +118,78 @@ namespace
 	void checkNSatisfyDependencies(
 		std::vector<boost::shared_ptr<Module> >& modules)
 	{
-		// FIXME: Hacer que funcione
+		using boost::shared_ptr;
+		using Err::FatalException;
+		typedef Module::Req TReq;
+		using Poco::UUID;
+		using std::map;
+		using std::vector;
+		
+		// Armo un índice de los módulos por ID
+		map<UUID, size_t> index;
+		for (int i = 0; i < modules.size(); i++)
+			index[modules[i]->GetID()] = i;
+
+		// Para cada módulo
+		for (size_t i = 0; i < modules.size(); i++)
+		{
+			// Si ya encontré que el módulo tenía dependencias imposibles de 
+			// satisfacer
+			if (!modules[i])
+				// Sigo con otro
+				continue;
+			
+			// Obtengo sus dependencias
+			const vector<TReq>& reqs = modules[i]->GetReqMods();
+
+			// Para cada dependencia
+			for (size_t j = 0; j < reqs.size(); j++)
+			{
+				// No está satisfecha?
+				if (index.find(reqs[j].first) == index.end() ||
+					modules[index[reqs[j].first]]->GetVersion()
+					< reqs[j].second)
+				{
+					// Marco como imposible de utilizar al módulo
+					modules[i] = shared_ptr<Module>();
+
+					// Salgo del for
+					break;
+				}
+			}
+
+			// Encontré dependencias no satisfechas con el módulo?
+			if (!modules[i])
+				// Empiezo desde el principio
+				i = 0; // ATENCIÓN!! Modifico el índice del loop
+		}
+
+		// Si llegué acá es porque todos los módulos estaban en alguna de dos
+		// situaciones:
+		// 1) Marcado como imposible de utilizar
+		// 2) Con todas sus dependencias posibles de encontrar
+		
+		// Satisfago las dependencias
+		for (size_t i = 0; i < modules.size(); i++)
+		{
+			// Si es null, pruebo con el siguiente
+			if (!modules[i])
+				continue;
+
+			// Busco los requerimientos
+			const vector<TReq>& reqVec = modules[i]->GetReqMods();
+			
+			// Armo el vector de requerimientos
+			vector<shared_ptr<Module> > reqs;
+			for (size_t j = 0; j < reqVec.size(); j++)
+				reqs.push_back(modules[index[reqVec[j].first]]);
+
+			// Lo entrego al módulo
+			if (!modules[i]->GiveReqMods(reqs))
+				throw FatalException(
+				"Error al satisfacer los requermientos del módulo " +
+				modules[i]->GetName() + ".");
+		}
 	}
 }
 
