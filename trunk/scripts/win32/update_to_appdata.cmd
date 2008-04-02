b55ca86ecab33db5537cb84829a5a2fd468d8b20
mchouza <mchouza@b858013c-4649-0410-a850-dde43e08a396>
2008-04-02 01:53:52 +0000

    - Logo actualizado para que tenga 75 pixels de altura (ngpd_logo.png). - Archivo de prueba eliminado por ser innecesario (test.html). - Templates actualizados para hacerlos cumplir el estándar XHTML 1.0 y para que utilicen escapes HTML (header.tpl, log.tpl y page_header.tpl). - Agregado el titulo a la página (template_utils.cpp). - Ahora copia los archivos de APPDATA al directorio correcto, el compartido (ya que es un servicio/daemon) (update_to_appdata.cmd).

--- trunk/scripts/win32/update_to_appdata.cmd
+++ trunk/scripts/win32/update_to_appdata.cmd
@@ -1,5 +1,8 @@
 @echo off
+rem Aparentemente, no tengo COMMON_APPDATA, así que hay que improvisar:
+rem En inglés sería: set COMMON_APPDATA=%ALLUSERSPROFILE%\Application data\
+set COMMON_APPDATA=%ALLUSERSPROFILE%\Datos de programa\
 echo Actualizando archivos...
-mkdir "%APPDATA%\NGPD" 2>NUL
-xcopy /EY "..\..\APPDATA\NGPD" "%APPDATA%\NGPD"
+mkdir "%COMMON_APPDATA%\NGPD" 2>NUL
+xcopy /EY "..\..\APPDATA\NGPD" "%COMMON_APPDATA%\NGPD"
 pause
